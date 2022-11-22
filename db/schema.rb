941fbcd1274930c618765cb0303fd3dcacb5653e
akirago218 <akinrapoo.218@gmail.com>
2022-11-22 13:59:14 +0900

    itemsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_22_014222) do
+ActiveRecord::Schema.define(version: 2022_11_22_043555) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2022_11_22_014222) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.text "content"
+    t.integer "category"
+    t.integer "status"
+    t.integer "payer"
+    t.integer "prefecture"
+    t.integer "shipping_day"
+    t.integer "price"
+    t.bigint "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
