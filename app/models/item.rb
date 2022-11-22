dffb0356c50cac4b15f1383a2744681df95860c8
akirago218 <akinrapoo.218@gmail.com>
2022-11-22 14:13:47 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :payer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :, numericality: { other_than: 1 , message: "can't be blank"}
+  
+  VALID_PRICEL_HALF = /\A[0-9]+\z/
+  validates  :price,  presence: true, format: {with: VALID_PRICEL_HALF},length: {minimum: 3, maxinum: 7},numericality: { only_integer: true,
+    greater_than: 300, less_than: 10000000
+    
 end
