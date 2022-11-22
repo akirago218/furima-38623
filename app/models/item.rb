a68b400b672af735e8f7dd91d276e6648f620e08
akirago218 <akinrapoo.218@gmail.com>
2022-11-22 12:13:04 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :payer
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
 end
