d29491d7dbfd50aecacb2162431ed0184fd6c794
akirago218 <akinrapoo.218@gmail.com>
2022-11-24 07:22:36 +0900

    エラーメッセージ編集

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -5,7 +5,7 @@ class OrderBuyer
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g 123-4567)" }
     validates :refecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
     validates :address
